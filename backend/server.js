8ccb4ad056421bf75d8de18c45ab3000376af51a
Priyam <priyamk778@gmail.com>
2025-07-19 13:36:03 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app=express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // Replace with your frontend URL
+    origin: "https://finance-flow-frontend.onrender.com", // Replace with your frontend URL
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,

0c313a9fd86533270345065611116c26c751d7b9
Priyam <priyamk778@gmail.com>
2025-07-19 13:20:35 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -29,5 +29,9 @@ app.use("/api/v1/dashboard",dashboardRoutes);
 
 app.use("/uploads",express.static(path.join(__dirname,"uploads")));
 
+app.get('/', (req, res) => {
+  res.send('API is live!');
+});
+
 const PORT=process.env.PORT || 5000;
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));

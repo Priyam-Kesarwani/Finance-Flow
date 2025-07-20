1163c9a6b503141215cdf3c039bd36f07fa9debf
Priyam <priyamk778@gmail.com>
2025-07-20 13:26:57 +0530

    Initial commit

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://finance-flow-backend-roog.onrender.com";
-export const BASE_URL = "http://localhost:8000" || "https://finance-flow-backend-roog.onrender.com";
+export const BASE_URL = "https://finance-flow-backend-roog.onrender.com";
+// export const BASE_URL = "http://localhost:8000" 
 
 
 export const API_PATHS = {
